e17339f51517d3f923b6091d52c9327f984179a6
sumanghimire <sumanghimire00@gmail.com>
2024-11-28 15:35:21 +0545

    feat: working on dashboard

--- modules/users/controllers/userDashboard.js
+++ modules/users/controllers/userDashboard.js
@@ -1,9 +1,17 @@
-const userDashboard = (req, res) => {
+const mongoose = require("mongoose");
+const userDashboard = async(req, res) => {
+
+    const User =mongoose.model("users");
+
+    const getUserData =await User.findOne({
+        _id: req.user._id,
+    }).select("balance name");
 
     console.log(req.user);
     res.status(200).json({
-        status: "This is dashboard!",
+        data: getUserData,
     });
+    
 };
 
 module.exports = userDashboard;
--- modules/users/controllers/userLogin.js
+++ modules/users/controllers/userLogin.js
@@ -1,52 +1,53 @@
 const mongoose = require("mongoose");
-const bcrypt =require("bcrypt");
-const jwt =require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const userLogin =async(req,res)=>{
+const userLogin = async (req, res) => {
 
     const Users = mongoose.model("users");
 
-    const {email,password}=req.body;
+    const { email, password } = req.body;
 
 
     // validation
-      try{
-        if(!email) throw "Please provide a email";
-        if(!password) throw "Please provide a password";
+    try {
+        if (!email) throw "Please provide a email";
+        if (!password) throw "Please provide a password";
 
 
         // checking for email and pasword is available on database or not???
 
         const getUser = await Users.findOne({
-                email:email,
+            email: email,
         });
-        if(!getUser) throw "User doesnot exits";
-        const matched =await bcrypt.compare(password,getUser.password);
+        if (!getUser) throw "User doesnot exits";
+        const matched = await bcrypt.compare(password, getUser.password);
 
-        if(!matched) throw "Email and Password do not match";
+        if (!matched) throw "Email and Password do not match";
 
-      }
-      catch(e){
+    }
+    catch (e) {
         res.status(400).json({
-            status:"failed",
-            message:e,
+            status: "failed",
+            message: e,
         });
         return;
-      };
+    };
 
     // sucessfull methods
-    
+
     const getUserForAccessToken = await Users.findOne({
-        email:email,
-})
-    const accessToken= jwt.sign({
-        email:getUserForAccessToken.email,
+        email: email,
+    })
+    const accessToken = jwt.sign({
+        _id: getUserForAccessToken._id,
+        email: getUserForAccessToken.email,
         name: getUserForAccessToken.name,
     },
-   process.env.jwt_salt,{
-    expiresIn:"90 days"
-   },
-);
+        process.env.jwt_salt, {
+        expiresIn: "90 days"
+    },
+    );
 
     res.status(200).json({
         status: "User logged in successfully!!",
@@ -54,4 +55,4 @@ const userLogin =async(req,res)=>{
     });
 };
 
-module.exports= userLogin;
+module.exports = userLogin;
